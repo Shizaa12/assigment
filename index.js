e5d6631cde759c8f1a83fb0f570d83667e2e50cc
shiza <shiza7432@gmail.com>
2024-08-16 01:42:58 -0700

    funtion of arrays

---
+++ index.js
@@ -0,0 +1,137 @@
+// function of lenghtI()
+ let fruits = ["Banana", "Orange", "Apple", "Mango"];
+ console.log(fruits);
+ console.log(fruits.length);
+
+
+
+
+//  example of toString()
+ let fruit = ["Banana", "Orange", "Apple", "Mango"];
+ console.log(fruit);
+ console.log(fruit.toString());
+
+
+
+
+
+//  example of at()
+ let fruit2 = ["Banana", "Orange", "Apple", "Mango"];
+ console.log(fruit2);
+ console.log(fruit2.at(3));
+
+
+
+
+//  example of join()
+let fruit3 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruit3);
+console.log(fruit2.join("^"));
+console.log(fruit2.join("*"));
+
+
+
+
+// example of pop()
+let items1=["ayesha","fatima","faiza","bilawal"]
+console.log(items1)
+console.log(items1.pop(2));
+
+
+
+
+
+// example of Push()
+let items2=["ayesha","fatima","faiza","bilawal"]
+console.log(items2)
+items2.push("iqra")
+console.log(items2)
+
+
+
+
+
+
+
+// example of shift()
+//
+
+
+
+
+
+
+// example of unshift()
+let item3=["ayesha","fatima","faiza","bilawal"]
+console.log(item3)
+console.log(item3.unshift("ali"));
+
+
+
+
+
+
+// example of delete()
+let item4=["ayesha","fatima","faiza","bilawal"]
+console.log(item4)
+console.log(item4.delete);
+
+
+
+
+
+
+// example of concat()
+ let myGirls = ["Cecilie", "Lone"];
+let myBoys = ["Emil", "Tobias", "Linus"];
+console.log(myGirls.concat(myBoys));
+
+
+
+
+
+
+
+
+// example of copyWithin()
+let myBoy = ["Emil", "Tobias", "Linus"];
+console.log(myBoy.copyWithin(1));
+
+
+
+
+
+
+// Example of flat ()
+ let myArr = [[1,2],[3,4],[5,6]];
+console.log(myArr);
+console.log(myArr.flat());
+// console.log(myArr.flatMap(x => [x, x * 10])); issue ha
+
+
+
+
+
+
+
+
+
+// example of splice () and toSpliced 
+let fruitsname = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruitsname);
+console.log(fruitsname.splice("2"));
+console.log(fruitsname.toSpliced("2"));
+
+
+
+
+
+
+// example of slice()
+let fruitsname1 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruitsname1);
+console.log(fruitsname1.slice("2"));
+
+
+
+
